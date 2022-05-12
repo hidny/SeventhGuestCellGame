d138cc80a8fa75d9b74238b8b7f32443d66e3f01
hidny <mtardibuono@gmail.com>
2022-05-12 02:48:42 -0400

    Initial commit for code to solve the 7th guest cell game

---
+++ src/Players/PlayerI.java
@@ -0,0 +1,13 @@
+package Players;
+
+import env.PositionCellGame;
+
+//import Position;
+
+public interface PlayerI {
+
+	public int getMove(PositionCellGame pos);
+	
+	public String getPlayerName();
+	
+}
